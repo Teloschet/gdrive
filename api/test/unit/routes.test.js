45b84f6bd8b46dd50bb2de95bf13c9d75fc0148e
Teloschet <chrisaleiro@gmail.com>
2023-07-26 13:12:27 -0300

    test: post test and prettier

--- api/test/unit/routes.test.js
+++ api/test/unit/routes.test.js
@@ -1,123 +1,174 @@
-import { describe, test, expect, jest } from '@jest/globals'
-import Routes from '../../src/routes.js'
+import {
+  describe,
+  test,
+  beforeEach,
+  expect,
+  jest
+} from '@jest/globals'
+import { logger } from '../../src/logger.js'
+import UploadHandler from '../../src/uploadHandler.js'
+import TestUtil from '../_util/testUtil.js'
+import Routes from './../../src/routes.js'
 
-describe('#Routes suite test', () => {
+describe('#Routes test suite', () => {
+  beforeEach(() => {
+      jest.spyOn(logger, 'info')
+          .mockImplementation()
+  })
+
+  const request = TestUtil.generateReadableStream(['some file bytes'])
+  const response = TestUtil.generateWritableStream(() => { })
+
   const defaultParams = {
-    request: {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      },
-      method: '',
-      body: {}
-    },
-    response: {
-      setHeader: jest.fn(),
-      writeHead: jest.fn(),
-      end: jest.fn()
-    },
-    values: () => Object.values(defaultParams)
+      request: Object.assign(request, {
+          headers: {
+              'Content-Type': 'multipart/form-data'
+          },
+          method: '',
+          body: {}
+      }),
+      response: Object.assign(response, {
+          setHeader: jest.fn(),
+          writeHead: jest.fn(),
+          end: jest.fn()
+      }),
+      values: () => Object.values(defaultParams)
   }
 
   describe('#setSocketInstance', () => {
-    test('setSocket should store io instance', () => {
-      const routes = new Routes()
-      const ioObj = {
-        to: (id) => ioObj,
-        emit: (event, message) => {}
-      }
+      test('setSocket should store io instance', () => {
+          const routes = new Routes()
+          const ioObj = {
+              to: (id) => ioObj,
+              emit: (event, message) => { }
+          }
 
-      routes.setSocketInstance(ioObj)
-      expect(routes.io).toStrictEqual(ioObj)
-    })
+          routes.setSocketInstance(ioObj)
+          expect(routes.io).toStrictEqual(ioObj)
+      })
   })
 
   describe('#handler', () => {
-    test('given an inexistent route it should choose default route', async () => {
-      const routes = new Routes()
-      const params = {
-        ...defaultParams
-      }
 
-      params.request.method = 'inexistent'
-      await routes.handler(...params.values())
-      expect(params.response.end).toHaveBeenCalledWith('hello world')
-    })
 
-    test('it should set any request with CORS enabled', async () => {
-      const routes = new Routes()
-      const params = {
-        ...defaultParams
-      }
+      test('given an inexistent route it should choose default route', async () => {
+          const routes = new Routes()
+          const params = {
+              ...defaultParams
+          }
 
-      params.request.method = 'inexistent'
-      await routes.handler(...params.values())
-      expect(params.response.setHeader).toHaveBeenCalledWith('Access-Control-Allow-Origin', '*')
-    })
+          params.request.method = 'inexistent'
+          await routes.handler(...params.values())
+          expect(params.response.end).toHaveBeenCalledWith('hello world')
+      })
 
-    test('given method OPTIONS it should choose options route', async () => {
-      const routes = new Routes()
-      const params = {
-        ...defaultParams
-      }
+      test('it should set any request with CORS enabled', async () => {
+          const routes = new Routes()
+          const params = {
+              ...defaultParams
+          }
 
-      params.request.method = 'OPTIONS'
-      await routes.handler(...params.values())
-      expect(params.response.writeHead).toHaveBeenCalledWith(204)
-      expect(params.response.end).toHaveBeenCalled()
-    })
+          params.request.method = 'inexistent'
+          await routes.handler(...params.values())
+          expect(params.response.setHeader)
+              .toHaveBeenCalledWith('Access-Control-Allow-Origin', '*')
+      })
 
-    test('given method POST it should choose post route', async () => {
-      const routes = new Routes()
-      const params = {
-        ...defaultParams
-      }
+      test('given method OPTIONS it should choose options route', async () => {
+          const routes = new Routes()
+          const params = {
+              ...defaultParams
+          }
 
-      params.request.method = 'POST'
+          params.request.method = 'OPTIONS'
+          await routes.handler(...params.values())
+          expect(params.response.writeHead).toHaveBeenCalledWith(204)
+          expect(params.response.end).toHaveBeenCalled()
+      })
 
-      jest.spyOn(routes, routes.post.name).mockResolvedValue()
+      test('given method POST it should choose post route', async () => {
+          const routes = new Routes()
+          const params = {
+              ...defaultParams
+          }
 
-      await routes.handler(...params.values())
-      expect(routes.post).toHaveBeenCalled()
-    })
+          params.request.method = 'POST'
+          jest.spyOn(routes, routes.post.name).mockResolvedValue()
 
-    test('given method GET it should choose get route', async () => {
-      const routes = new Routes()
-      const params = {
-        ...defaultParams
-      }
+          await routes.handler(...params.values())
+          expect(routes.post).toHaveBeenCalled()
 
-      params.request.method = 'GET'
+      })
+      test('given method GET it should choose get route', async () => {
+          const routes = new Routes()
+          const params = {
+              ...defaultParams
+          }
+          jest.spyOn(routes, routes.get.name).mockResolvedValue()
 
-      jest.spyOn(routes, routes.get.name).mockResolvedValue()
+          params.request.method = 'GET'
+          await routes.handler(...params.values())
+          expect(routes.get).toHaveBeenCalled()
 
-      await routes.handler(...params.values())
-      expect(routes.get).toHaveBeenCalled()
-    })
+      })
   })
 
   describe('#get', () => {
-    test('given method GET it should list all files downloaded', async () => {
-      const route = new Routes()
-      const params = {
-        ...defaultParams
-      }
-      const fileStatusMock = [
-        {
-          size: "14.1 kB",
-          lastModified: '2023-07-24T19:56:13.812Z',
-          owner: 'telos',
-          file: 'file.png'
-        }
-      ]
+      test('given method GET it should list all files downloaded', async () => {
+          const routes = new Routes()
+          const params = {
+              ...defaultParams
+          }
 
-      jest.spyOn(route.fileHelper, route.fileHelper.getFilesStatus.name)
-        .mockResolvedValue(fileStatusMock)
+          const filesStatusesMock = [
+              {
+                  size: "188 kB",
+                  lastModified: '2021-09-03T20:56:28.443Z',
+                  owner: 'erickwendel',
+                  file: 'file.txt'
+              }
+          ]
+          jest.spyOn(routes.fileHelper, routes.fileHelper.getFilesStatus.name)
+              .mockResolvedValue(filesStatusesMock)
 
-      params.request.method = 'GET'
-      await route.handler(...params.values())
+          params.request.method = 'GET'
+          await routes.handler(...params.values())
 
-      expect(params.response.writeHead).toHaveBeenCalledWith(200)
-      expect(params.response.end).toHaveBeenCalledWith(JSON.stringify(fileStatusMock))
-    })
+
+          expect(params.response.writeHead).toHaveBeenCalledWith(200)
+          expect(params.response.end).toHaveBeenCalledWith(JSON.stringify(filesStatusesMock))
+
+      })
+  })
+
+  describe('#post', () => {
+      test('it should validate post route workflow', async () => {
+          const routes = new Routes('/tmp')
+          const options = {
+              ...defaultParams
+          }
+          options.request.method = 'POST'
+          options.request.url = '?socketId=10' 
+
+           
+          jest.spyOn(
+              UploadHandler.prototype,
+              UploadHandler.prototype.registerEvents.name
+          ).mockImplementation((headers, onFinish) => {
+              const writable = TestUtil.generateWritableStream(() => {})
+              writable.on("finish", onFinish)
+
+              return writable
+          })
+
+          await routes.handler(...options.values())
+
+          expect(UploadHandler.prototype.registerEvents).toHaveBeenCalled()
+          expect(options.response.writeHead).toHaveBeenCalledWith(200)
+
+          const expectedResult = JSON.stringify({ result: 'Files uploaded with success! ' })
+          expect(options.response.end).toHaveBeenCalledWith(expectedResult)
+
+      })
   })
 })
